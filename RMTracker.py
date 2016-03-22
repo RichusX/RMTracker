c8aaf43d971ad3827ade65ba9f2dc615001334a8
RichusX <pyromaniaclv@gmail.com>
2016-03-22 02:10:05 +0000

    Added continuous check for updates

--- RMTracker.py
+++ RMTracker.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import dryscrape
 import sys
+import time
 from pushbullet.pushbullet import PushBullet
 
 if 'linux' in sys.platform:
     dryscrape.start_xvfb()
 
-trackingNumber = 'YOUR_TRACKING_NUMBER_HERE'
-apiKey = 'YOUR_PUSHBULLET_API_KEY_HERE'
+trackingNumber = 'YOUR_TRACKING_NUMBER'
+apiKey = 'YOUR_API_KEY'
 p = PushBullet(apiKey)
+refreshRate = 30 # how often to check the status in minutes
 
 # set up a web scraping session
 sess = dryscrape.Session(base_url = 'https://www.royalmail.com')
@@ -16,15 +18,22 @@ sess = dryscrape.Session(base_url = 'https://www.royalmail.com')
 # we don't need images
 sess.set_attribute('auto_load_images', False)
 
-# visit the tracking page and lookup the tracking number
-sess.visit('/track-your-item')
-q = sess.at_xpath('//*[@name="tracking_number"]')
-q.set(trackingNumber)
-q.form().submit()
+while True:
+    # visit the tracking page and lookup the tracking number
+    sess.visit('/track-your-item')
+    q = sess.at_xpath('//*[@name="tracking_number"]')
+    q.set(trackingNumber)
+    q.form().submit()
 
-#extract the tracking status
-x = sess.at_xpath('//*[@class="status result-row padding20lr first"]')
-status = x.text()
-
-print status
-p.pushNote("YOUR_DEVICE_IDEN_HERE", 'RoyalMail Tracking Status', status)
+    # extract the tracking status
+    x = sess.at_xpath('//*[@class="status result-row padding20lr first"]')
+    status = x.text()
+    prevStatus = ''
+    
+    if (status != prevStatus):
+        # push the status to PushBullet
+        p.pushNote("YOUR_DEVICE_IDENT", 'RoyalMail Tracking', status)
+        prevStatus = status
+    
+    # wait for 'refreshRate' minutes before checking again
+    time.sleep(refreshRate * 60)
